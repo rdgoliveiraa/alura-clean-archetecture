7fcd04bbc82365deda4bbe41087679d685e09e05
Rodrigo Oliveira de Almeida <rdg.oliveiraa@gmail.com>
2021-08-14 08:46:31 -0300

    Aula-4 criacao de repositorios de alunos e separacao com separacao de camadas de dominio e arquitetura

--- src/main/java/br/com/alura/escola/dominio/aluno/Email.java
+++ src/main/java/br/com/alura/escola/dominio/aluno/Email.java
@@ -4,6 +4,14 @@ public class Email {
 
     private String endereco;
 
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
     // VALUE OBJECT
     public Email(String endereco) {
         if (endereco == null || !endereco.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
